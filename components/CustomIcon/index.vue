e579ff4c11d790b1a564343a95ea5a42e232b9ea
cuong.nguyen <cuongnm@metric.vn>
2024-06-26 17:46:02 +0700

    [feat-conact-us-screen][feat] add banner & infor component

--- components/CustomIcon/index.vue
+++ components/CustomIcon/index.vue
@@ -74,6 +74,8 @@ const iconMappings = {
 
 .size-24 {
   width: 24px;
+  min-width: 24px;
   height: 24px;
+  min-height: 24px;
 }
 </style>
--- helpers/constains.ts
+++ helpers/constains.ts
@@ -24,3 +24,22 @@ export const MENUS = [
     to: NAVIGATIONS.pricing
   }
 ];
+
+export const CONTACTS = [
+  {
+    label: "CÔNG TY CỔ PHẦN KHOA HỌC DỮ LIỆU",
+    icon: "Building"
+  },
+  {
+    label: "033.806.2221",
+    icon: "Phone"
+  },
+  {
+    label: "info@metric.vn",
+    icon: "Envelope"
+  },
+  {
+    label: "Tầng 6 Tòa nhà AZ Lâm Viên, 107A Nguyễn Phong Sắc, phường Dịch Vọng Hậu, quận Cầu Giấy, thành phố Hà Nội, Việt Nam",
+    icon: "MapPin"
+  }
+];
