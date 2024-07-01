67d99e6a42b3a58310392ec64a428d5457e760fe
cuong.nguyen <cuongnm@metric.vn>
2024-07-01 10:28:25 +0700

    [fix] wrong css

--- components/header/navbar/index.vue
+++ components/header/navbar/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="headerNavbar">
     <div class="headerNavbarMenu">
-      <AButton v-for="(item, index) in MENUS" :key="index" type="text" :class="isDarkBlueHeader ? 'headerText' : 'headerTextDarkBlue'" @click="navigateTo(item.to)">{{ item.label }}</AButton>
+      <AButton v-for="(item, index) in MENUS" :key="index" type="text" :class="{ headerText: isDarkBlueHeader, headerTextDarkBlue: !isDarkBlueHeader }" @click="navigateTo(item.to)">{{ item.label }}
+      </AButton>
     </div>
     <div class="headerNavbarCallButton">
       <AButton @click="navigateTo(NAVIGATIONS.signIn)">Đăng nhập</AButton>
