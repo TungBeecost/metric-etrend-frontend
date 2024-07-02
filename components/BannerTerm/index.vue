f21a2540dfc250afaecb2fa43342f46165165d46
cuong.nguyen <cuongnm@metric.vn>
2024-07-02 14:07:22 +0700

    [feat-term-policy][feat] add infor guard term

---
+++ components/BannerTerm/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bannerWrapper">
+    <p class="title">{{ title }}</p>
+    <CustomIcon :type="iconType" :is-custom-size="true" class="icon" />
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import type { TypeIcon } from '../CustomIcon/index.vue';
+
+defineProps<{
+  title: string,
+  iconType: TypeIcon
+}>()
+</script>
+
+<style lang="scss" scoped>
+@import url("./index.scss")
+</style>
---
+++ components/SidebarTerm/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sidebar">
+    <div v-for="item in TERM_AND_POLICIES" class="menuItem" :class="{ active: path.includes(item.link) }" @click="navigateTo(item.link)">
+      <CustomIcon type="Docs" class="menuIcon" />
+      <p class="menuLabel">{{ item.label }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TERM_AND_POLICIES } from '../../constant/constains';
+
+const { path } = useRoute();
+</script>
+
+<style lang="scss" scoped>
+@import url("./index.scss");
+</style>
