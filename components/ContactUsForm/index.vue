1789231b08fb75e7fdd838e833b117543c46e54d
cuongle <pau30012002@gmail.com>
2024-07-12 10:40:52 +0700

    fix_bug_UI

--- components/ContactUsForm/index.vue
+++ components/ContactUsForm/index.vue
@@ -40,8 +40,6 @@ interface IAdditionalInfo {
   utmCampaign: string;
   isMobile: boolean;
   firstVisit: string;
-  mkLeadSource: Array<string>;
-  mkUserDemand: string;
 }
 
 interface IFormValue {
@@ -52,21 +50,40 @@ interface IFormValue {
   organization_name: string;
   category: string;
   note: string;
-  label_init?: string | 'nóng';
-  source_name?: string | 'e-report';
+  label_init?: string;
+  source_name?: string;
   socialMediaType: string;
-  campaign: string | 'Đăng ký dùng thử';
+  campaign: string;
   additional_info: IAdditionalInfo;
 }
 
-function getCookie(name: any) {
+function getCookie(name: string): string | null {
+  if (typeof document === 'undefined') {
+    console.warn('getCookie was called server-side. Cookies can only be accessed in a browser environment.');
+    return null;
+  }
+
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+  if (parts.length === 2) {
+    return parts.pop()?.split(';').shift() ?? null;
+  }
+  return null;
 }
 
-const urlReferrerValue = getCookie('urlReferrer');
+const getCurrentURL = () => {
+  if (typeof window !== 'undefined') {
+    const baseUrl = new URL(window.location.href);
+    return `${baseUrl.protocol}//${baseUrl.host}/`;
+  } else {
+    return process.env.BASE_URL || 'http://localhost:3000/';
+  }
+};
 
+const route = useRoute();
+const routeName = String(route.name) || "";
+const utm_medium = getCookie('utm_campaign');
+console.log('utm_medium', utm_medium);
 
 const formValues = reactive<IFormValue>({
   name: "",
@@ -75,21 +92,19 @@ const formValues = reactive<IFormValue>({
   organization_name: "metric",
   category: "",
   note: "",
-  label_init: "",
+  label_init: "nóng",
   source_name: "e-report",
-  campaign: "Đăng ký dùng thử",
+  campaign: 'Đăng ký dùng thử',
   companyType: "",
   socialMediaType: "",
   additional_info: {
-    urlReferrer: "",
-    url: "",
-    routeName: "",
-    utmMedium: "",
-    utmCampaign: "",
-    isMobile: false,
-    firstVisit: "",
-    mkLeadSource: [],
-    mkUserDemand: "",
+    urlReferrer: getCookie('urlReferrer') || "",
+    url: getCurrentURL(),
+    routeName: routeName || "",
+    utmMedium: getCookie('utm_medium') || "",
+    utmCampaign: getCookie('utm_campaign') || "",
+    isMobile: getCookie('is_mobile') === 'true',
+    firstVisit: getCookie('first_visit') || "",
   },
 });
 const errors = useState<Partial<IFormValue>>(() => ({}));
@@ -132,13 +147,23 @@ const validateForm = async () => {
           email: information.email,
           phone: information.phone,
           organization_name: information.organization_name,
-          note: `From: ${information.additional_info.url}\nfirst_visit: ${information.additional_info.firstVisit}\nurl_referrer: ${information.additional_info.urlReferrer}\nemailProfile: ${information.email}\nlead_source: ${information.additional_info.mkLeadSource.join(',')}\nuser_demand: ${information.category}\ncompany_type: ${information.companyType}`,
+          note: `from: ${information.additional_info.url}\nfirst_visit: ${information.additional_info.firstVisit}\npub: ${information.additional_info.routeName}\nutm_medium: ${information.additional_info.utmMedium}\nutm_campaign: ${information.additional_info.utmCampaign}\nurl_referrer: ${information.additional_info.urlReferrer}\nnlead_source: ${information.socialMediaType}\nuser_demand: ${information.category}\ncompany_type: ${information.companyType}`,
           label_init: information.label_init,
           source_name: information.source_name,
           campaign: information.campaign,
           additional_info: {
-            ...information.additional_info,
-            urlReferrer: urlReferrerValue,
+            urlReferrer: information.additional_info.urlReferrer,
+            url: information.additional_info.url,
+            routeName: information.additional_info.routeName,
+            utmMedium: information.additional_info.utmMedium,
+            utmCampaign: information.additional_info.utmCampaign,
+            isMobile: information.additional_info.isMobile,
+            firstVisit: information.additional_info.firstVisit,
+            mkLeadSource: [
+              information.socialMediaType,
+            ],
+            mkUserDemand: information.category,
+            mkUserDemandmkUserDemand: information.companyType,
           },
         },
         {
