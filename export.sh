ef723abef5e12e2f1c692ded00d7c63352b3dc79
Doctor-Sentry <quyvn@metric.vn>
2024-07-19 17:29:18 +0700

    Update ssr build

--- export.sh
+++ export.sh
@@ -2,10 +2,10 @@
 set -o allexport
 source .env
 set +o allexport
-if [[ "SSR" == "true" ]]; then
-    export NUXT_APP_CDN_URL=${BASE_URL}
-else
-    export NUXT_APP_CDN_URL=${BASE_URL}/ssr 
-fi
+# if [[ "SSR" == "true" ]]; then
+#     export NUXT_APP_CDN_URL=${BASE_URL}
+# else
+#     export NUXT_APP_CDN_URL=${BASE_URL}/ssr 
+# fi
 
 npm run start 
