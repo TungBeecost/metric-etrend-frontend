0484c921520acc2e45959976da9f519c6c035de6
Tien Minh <minhtien@metric.vn>
2024-07-19 15:56:15 +0700

    ssr

--- plugins/antd.ts
+++ plugins/antd.ts
@@ -5,3 +5,10 @@ import Antd from 'ant-design-vue';
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(Antd)
 })
+
+
+// import Vue3Marquee from "vue3-marquee";
+//
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(Vue3Marquee, { name: "Vue3Marquee" });
+// });
