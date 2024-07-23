70488c4c92199ac767b773225fade4bfa1e9ba5d
Tien Minh <minhtien@metric.vn>
2024-07-23 13:38:17 +0700

    fix text payments

--- components/ContactUs/index.vue
+++ components/ContactUs/index.vue
@@ -81,7 +81,7 @@ const handleSubmitLeadForm = async () => {
   const utm_term = variables?.utm_term || ''
   const utm_content = variables?.utm_content || ''
   const url_referrer = variables?.url_referrer || ''
-  const is_mobile = variables?.is_mobile ? 'true' : 'false'
+  const is_mobile = !!variables?.is_mobile
   const pub = variables?.pub || ''
   const emailProfile = formData.value.email || userInfo.value?.email || ''
   const first_visit = localStorage.getItem('first_visit') || ''
@@ -90,7 +90,7 @@ const handleSubmitLeadForm = async () => {
   const mkCompanyType = formData.value.mktCompanyType || ''
   let note = `From: ${window.location.href}\n`
   note += `\nfirst_visit: ${first_visit}\npub: ${pub}\nutm_source: ${utm_source} utm_medium: ${utm_medium} utm_campaign: ${utm_campaign} utm_term: ${utm_term} utm_content: ${utm_content} url_referrer: ${url_referrer}\nemailProfile: ${emailProfile}\n`
-  note += `lead_source: ${mktLeadSource.join(',')}\nuser_demand: ${mkUserDemand}\ncompany_type: ${mkCompanyType}\nis_mobile=${is_mobile}`
+  note += `lead_source: ${mktLeadSource.join(',')}\nuser_demand: ${mkUserDemand}\ncompany_type: ${mkCompanyType}\ndevice: ${is_mobile ? 'mobile' : 'desktop'}`
   console.log('note', formData.value.name)
 
   const payload = {
