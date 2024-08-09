54e00c4f932379497fcbb412f563aaa2c3d492be
cuongle <pau30012002@gmail.com>
2024-08-09 17:01:21 +0700

    change_title_payment

--- plugins/gtm-tracking.js
+++ plugins/gtm-tracking.js
@@ -1,14 +1,22 @@
 export default ({ app }) => {
-    app.router.afterEach((to, from) => {
-        if (window && window.dataLayer) {
-            window.dataLayer.push({
-                event: 'pageview',
-                page: {
-                    path: to.fullPath,
-                    name: to.name,
-                    title: document.title,
-                },
+    if (import.meta.client) {
+        if (app && app.router) {
+            app.router.afterEach((to, from) => {
+                if (window && window.dataLayer) {
+                    window.dataLayer.push({
+                        event: 'pageview',
+                        page: {
+                            path: to.fullPath,
+                            name: to.name,
+                            title: document.title,
+                        },
+                    });
+                }
             });
+        } else {
+            console.error('Router is not defined');
         }
-    });
+    } else {
+        console.error('Not running on the client side');
+    }
 };
