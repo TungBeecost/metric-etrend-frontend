db5bcbcd266e7ed2ec08ce8c7e4fa981719d477d
cuongle <pau30012002@gmail.com>
2024-08-17 17:46:20 +0700

    fix_bug

--- pages/view_pdf/[slug].vue
+++ pages/view_pdf/[slug].vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import { createLoadingTask, VuePdf } from 'vue3-pdfjs';
 import axios from 'axios';
 import HeaderDeptReport from '~/components/report/HeaderDeptReport.vue';
@@ -15,7 +15,6 @@ const loadingPercentage = ref(0);
 const pdfSrc = ref('');
 const numOfPages = ref(0);
 const currentPage = ref(1);
-const isMobile = ref(false);
 const remainingTime = ref(0);
 const reportName = ref('');
 const currentUserStore = useCurrentUser();
@@ -101,7 +100,6 @@ watch(numOfPages, () => {
   }, 100);
 });
 
-
 let lastScrollTop = 0;
 
 const handleScroll = () => {
@@ -134,6 +132,14 @@ const handleScroll = () => {
   lastScrollTop = scrollTop;
 };
 
+const isMobileDevice = computed(() => window.innerWidth <= 768);
+
+const onChangePage = (direction) => {
+  const newPage = currentPage.value + direction;
+  if (newPage > 0 && newPage <= numOfPages.value) {
+    currentPage.value = newPage;
+  }
+};
 
 onMounted(() => {
   if (!currentUserStore.authenticated) {
@@ -144,63 +150,119 @@ onMounted(() => {
   getReportPdfUrl(slug);
 
   window.addEventListener('scroll', handleScroll);
-//   window.addEventListener('resize', handleResize); // Add resize listener
   handleScroll();
 });
-//
+
 onUnmounted(() => {
   window.removeEventListener('scroll', handleScroll);
-//   window.removeEventListener('resize', handleResize); // Remove resize listener
 });
 </script>
 
 <template>
-  <div v-if="url_download" class="view_pdf">
-    <div class="header_pdf">
-      <HeaderDeptReport v-if="remainingTime" :num-of-pages="numOfPages" :current-page="currentPage"
-                        :remaining-time="calculateTargetDate(remainingTime)" :report-name="reportName"/>
-    </div>
-    <div class="container default_section">
-      <div class="mini_map">
-        <div
-            v-for="index in numOfPages"
-            :key="index"
-            class="mini_map_page"
-            style="width: 100%; height: 150px;"
-            @click="scrollToPage(index)"
-        >
-          <VuePdf
-              v-if="miniMapPagesInView.has(`${index}`)"
-              :src="pdfSrc"
-              :page="index"
-              :num-pages="numOfPages"
-              :enable-text-selection="false"
-              :enable-annotations="false"
-          />
+  <div v-if="url_download">
+    <div v-if="isMobileDevice">
+      <div v-if="isLoading"></div>
+      <div v-else style="width: 100%;">
+        <div style="width:100%;" class="pdf-wrapper">
+          <div v-for="index in numOfPages" style="width: 100%;position: relative;">
+            <VuePdf
+                v-if="currentPage === index"
+                :src="pdfSrc"
+                :page="currentPage"
+                :num-pages="numOfPages"
+                style="width: 100%;"
+            />
+            <ChartMask
+                v-if="isHideContent && currentPage === index && index >= 9"
+                subtitle="Vui lòng đăng ký để nhận và xem bản đầy đủ của báo cáo"
+                ok-button="Đăng ký nhận báo cáo"
+                :handle-unlock-report="handleUnlockReport"
+            />
+            <div
+                v-if="currentPage === index"
+                class="btn-control btn-prev"
+                :class="{disabled: currentPage === 1}"
+                @click="onChangePage(-1)"
+            >
+              <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19.4102 8.35254L12.4102 15.8525L19.4102 23.3525" stroke="#241E46" stroke-width="1.3"
+                      stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </div>
+            <div
+                v-if="currentPage === index"
+                class="btn-control btn-next"
+                :class="{disabled: currentPage === numOfPages}"
+                @click="onChangePage(1)"
+            >
+              <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12.5898 8.35254L19.5898 15.8525L12.5898 23.3525" stroke="#241E46" stroke-width="1.3"
+                      stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </div>
+          </div>
+          <div style="display: flex; flex-wrap: wrap; gap: 8px;justify-content: center; margin-top: 8px;">
+            <div
+                v-for="index in numOfPages"
+                style="width: 12px; height: 12px; background: #fff; border: 2px solid #fff; cursor: pointer;"
+                :style="{background: currentPage === index ? '#FF6931' : '#fff'}"
+                @click="currentPage = index"
+            />
+          </div>
+          <div
+              style="color: #fff;line-height: 1; margin-top: 12px; user-select: none; display: flex; align-items: center; justify-content: center; gap: 8px;">
+            {{ currentPage }} / {{ numOfPages }}
+          </div>
         </div>
       </div>
-      <div class="main_content">
-        <div v-if="isLoading"
-             style="width: 100%; height: 100vh; display: flex; align-items: center; justify-content: center">
-          <a-spin size="large"/>
-        </div>
-        <div v-else class="container_content" style="background: #525659">
+    </div>
+    <div v-else class="view_pdf">
+      <div class="header_pdf">
+        <HeaderDeptReport v-if="remainingTime" :num-of-pages="numOfPages" :current-page="currentPage"
+                          :remaining-time="calculateTargetDate(remainingTime)" :report-name="reportName"/>
+      </div>
+      <div class="container default_section">
+        <div class="mini_map">
           <div
               v-for="index in numOfPages"
               :key="index"
+              class="mini_map_page"
+              style="width: 100%; height: 150px;"
+              @click="scrollToPage(index)"
           >
+            <VuePdf
+                v-if="miniMapPagesInView.has(`${index}`)"
+                :src="pdfSrc"
+                :page="index"
+                :num-pages="numOfPages"
+                :enable-text-selection="false"
+                :enable-annotations="false"
+            />
+          </div>
+        </div>
+        <div class="main_content">
+          <div v-if="isLoading"
+               style="width: 100%; height: 100vh; display: flex; align-items: center; justify-content: center">
+            <a-spin size="large"/>
+          </div>
+          <div v-else class="container_content" style="background: #525659">
             <div
-                v-if="index === currentPage"
-                style="width: 100%; position: relative; padding: 24px;box-shadow: 0 0 10px rgba(0,0,0,0.1);"
+                v-for="index in numOfPages"
+                :key="index"
             >
-              <VuePdf
-                  :src="pdfSrc"
-                  :page="currentPage"
-                  :num-pages="numOfPages"
-                  :enable-text-selection="true"
-                  :enable-annotations="true"
-                  style="width: 100%;"
-              />
+              <div
+                  v-if="index === currentPage"
+                  style="width: 100%; position: relative; padding: 24px;box-shadow: 0 0 10px rgba(0,0,0,0.1);"
+              >
+                <VuePdf
+                    :src="pdfSrc"
+                    :page="currentPage"
+                    :num-pages="numOfPages"
+                    :enable-text-selection="true"
+                    :enable-annotations="true"
+                    style="width: 100%;"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -263,6 +325,53 @@ onUnmounted(() => {
     div {
       background: #EEEBFF;
     }
+  }
+}
+
+.pdf-wrapper {
+  position: relative;
+
+  padding: 16px;
+
+  border-radius: var(--radius-2xl, 16px);
+  background: var(--Gradient-2, linear-gradient(90deg, #FF6931 1.09%, #FF9839 99.23%));
+
+  .btn-control {
+    position: absolute;
+
+    top: 50%;
+
+    display: flex;
+    padding: 4px;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    border-radius: 10000px;
+    background: rgba(255, 255, 255, 0.20);
+    box-shadow: 10px 10px 40px 0px rgba(0, 0, 0, 0.15);
+    border: 1px solid #EEEBFF;
+    backdrop-filter: blur(2px);
+
+    cursor: pointer;
+
+    &:hover {
+      background: rgba(255, 255, 255, 0.90);
+    }
+
+    //&.disabled {
+    //  display: none;
+    //}
+  }
+
+  .btn-prev {
+    left: -15px;
+    transform: translateY(-50%);
+  }
+
+  .btn-next {
+    right: -15px;
+    transform: translateY(-50%);
   }
 }
 
