4e581037536d73b2a7fdb4fe5d21bf6730bcb8f6
Tien Minh <minhtien@metric.vn>
2024-09-18 13:43:36 +0700

    config SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,25 +5,25 @@ export default defineNuxtConfig({
     ssr: false,
     app: {
         head: {
-            title: "eReport - Kho báo cáo Ecommerce toàn diện",
+            // title: "eReport - Kho báo cáo Ecommerce toàn diện",
             htmlAttrs: {
                 lang: "vi",
             },
             meta: [
                 { charset: "utf-8" },
                 { name: "viewport", content: "width=device-width, initial-scale=1" },
-                {
-                    hid: "description",
-                    name: "description",
-                    content:
-                        "eReport là Kho báo cáo thị trường Ecommerce toàn diện giúp Doanh nghiệp, Thương hiệu và Nhà bán tiết kiệm thời gian nghiên cứu thị trường, giảm thiểu rủi ro đầu tư với dữ liệu chính xác nhất.",
-                },
-                {
-                    hid: "og:image",
-                    property: "og:image",
-                    content:
-                        "https://lh3.googleusercontent.com/pw/AP1GczMTZp0lf_VW7W_Y6n3qg602m-LlqfpCIeVX_i4D3pnqT6FiloK5hY86XfTsqVHd4xRRs9tSKppP6FZdEPWO_V8D_UEsfj8KEnChoiU7zyiwDlHzZaTeAePmGxcqzG98qDJ9bahok5MhwtDzp3EKTiI_=w1600-h900-s-no-gm",
-                },
+                // {
+                //     hid: "description",
+                //     name: "description",
+                //     content:
+                //         "eReport là Kho báo cáo thị trường Ecommerce toàn diện giúp Doanh nghiệp, Thương hiệu và Nhà bán tiết kiệm thời gian nghiên cứu thị trường, giảm thiểu rủi ro đầu tư với dữ liệu chính xác nhất.",
+                // },
+                // {
+                //     hid: "og:image",
+                //     property: "og:image",
+                //     content:
+                //         "https://lh3.googleusercontent.com/pw/AP1GczMTZp0lf_VW7W_Y6n3qg602m-LlqfpCIeVX_i4D3pnqT6FiloK5hY86XfTsqVHd4xRRs9tSKppP6FZdEPWO_V8D_UEsfj8KEnChoiU7zyiwDlHzZaTeAePmGxcqzG98qDJ9bahok5MhwtDzp3EKTiI_=w1600-h900-s-no-gm",
+                // },
                 { name: "google-site-verification", content: "-A5h4Bx3cBpC9vnJxfRvxvegNFZgMorMQlE6M76uLbc" },
             ],
             link: [
