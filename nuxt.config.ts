292d2e5b33a0ab5ed50681a331b7e49ca2d89727
Tien Minh <minhtien@metric.vn>
2024-10-24 09:29:46 +0700

    cdn url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     ssr: false, // Bật chế độ SSR
 
     app: {
+      cdnURL: process.env.URL_CDN,
       head: {
           title: "eReport - Kho báo cáo Ecommerce toàn diện",
           htmlAttrs: {
