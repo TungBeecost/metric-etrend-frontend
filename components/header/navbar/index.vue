6a12cacf7308d40ce1b8b2e938652e1ab2ff21ed
Tien Minh <minhtien@metric.vn>
2024-10-25 14:55:02 +0700

    ereport-migrate

--- components/header/navbar/index.vue
+++ components/header/navbar/index.vue
@@ -21,11 +21,14 @@
       </div>
     </div>
 
-    <a-modal class="button_login" :visible="currentUserStore.isShowPopupLogin"
-             :footer="false"
-             style="margin-top: 30px"
-             @ok="currentUserStore.setShowPopupLogin(false)"
-             @cancel="currentUserStore.setShowPopupLogin(false)">
+    <a-modal
+        class="button_login" :visible="currentUserStore.isShowPopupLogin"
+        :footer="false"
+        style="margin-top: 30px"
+        @ok="currentUserStore.setShowPopupLogin(false)"
+        @cancel="currentUserStore.setShowPopupLogin(false)"
+        destroy-on-close
+    >
       <login-button/>
     </a-modal>
   </div>
