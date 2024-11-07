cc3fc9dd165c38b88f7fdd456f5a2bc779a4a897
Tien Minh <minhtien@metric.vn>
2024-11-07 14:39:08 +0700

    value report from clickhouse

--- components/report/GeneralOverview.vue
+++ components/report/GeneralOverview.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue';
+import {computed, ref, onMounted} from 'vue';
 import SummaryStatistic from "~/components/report/SummaryStatistic.vue";
-import { formatSortTextCurrency } from "~/helpers/utils";
+import {formatSortTextCurrency} from "~/helpers/utils";
 import dayjs from "dayjs";
-import { formatNumber } from "~/helpers/FormatHelper";
+import {formatNumber} from "~/helpers/FormatHelper";
 
 const props = defineProps({
   data: {
     type: Object,
-    default: () => {},
+    default: () => {
+    },
   },
   isHideContent: {
     type: Boolean,
@@ -29,18 +30,18 @@ onMounted(() => {
 const hightestMonthRevenue = computed(() => {
   const byOverview = props.data?.data_analytic?.by_overview;
   if (!byOverview) {
-    return { revenue: 0 };
+    return {revenue: 0};
   }
-  const { lst_revenue_sale_monthly } = byOverview;
+  const {lst_revenue_sale_monthly} = byOverview;
   const highestMonthRevenue = lst_revenue_sale_monthly.reduce(
       (acc: { revenue: number }, item: { revenue: string | number }) => {
         const itemRevenue = typeof item.revenue === 'string' ? parseFloat(item.revenue) : item.revenue;
         if (itemRevenue > acc.revenue) {
-          return { ...item, revenue: itemRevenue };
+          return {...item, revenue: itemRevenue};
         }
         return acc;
       },
-      { revenue: 0 }
+      {revenue: 0}
   );
   return highestMonthRevenue;
 });
@@ -61,24 +62,13 @@ const formatDateFunc = (value: string, format: string) => {
   return dayjs(value, "YYYYMMDD").format(format);
 };
 
-const correctedSalesData = props.data?.data_analytic?.by_overview?.lst_revenue_sale_monthly
-    ?.slice()
-    .map((item: { sale: number, score?: number }) => {
-      if (item.sale !== undefined) {
-        return item.sale;
-      } else if (item.score !== undefined) {
-        return item.score;
-      } else {
-        return 0; // Default value if both are undefined
-      }
-    }) || [];
 
 const diffMonths = computed(() => {
   const filterCustom = props.data?.filter_custom;
   if (!filterCustom) {
     return "0 tháng";
   }
-  const { start_date, end_date } = filterCustom;
+  const {start_date, end_date} = filterCustom;
   const startDate = dayjs(start_date);
   const endDate = dayjs(end_date);
   return endDate.diff(startDate, "months") + 1 + " tháng";
@@ -87,9 +77,9 @@ const diffMonths = computed(() => {
 const diffRevenueMonths = computed(() => {
   const byOverview = props.data?.data_analytic?.by_overview;
   if (!byOverview) {
-    return { revenue: 0 };
+    return {revenue: 0};
   }
-  const { lst_revenue_sale_monthly } = byOverview;
+  const {lst_revenue_sale_monthly} = byOverview;
   const latestMonth = lst_revenue_sale_monthly.slice(-1);
   const previousMonth = lst_revenue_sale_monthly.slice(-2, -1);
   const diff = latestMonth[0].revenue - previousMonth[0].revenue;
@@ -104,9 +94,9 @@ const diffRevenueMonths = computed(() => {
 const diffHalfYear = computed(() => {
   const byOverview = props.data?.data_analytic?.by_overview;
   if (!byOverview) {
-    return { revenue: 0 };
+    return {revenue: 0};
   }
-  const { lst_revenue_sale_monthly } = byOverview;
+  const {lst_revenue_sale_monthly} = byOverview;
   const latestHalfYear = lst_revenue_sale_monthly.slice(-6);
   const previousHalfYear = lst_revenue_sale_monthly.slice(-12, -6);
 
@@ -160,7 +150,7 @@ const charts = computed(() => {
       },
       tooltip: {
         enabled: true,
-        formatter: function(this: { x: string, y: number, series: { name: string } }): string {
+        formatter: function (this: { x: string, y: number, series: { name: string } }): string {
           return `<b>${this.series.name}</b><br/>${this.x}: ${formatCurrency(this.y)}`;
         }
       },
@@ -203,7 +193,7 @@ const charts = computed(() => {
               fontWeight: 400,
               fontFamily: 'Inter'
             },
-            formatter: function(this: { value: number }): string {
+            formatter: function (this: { value: number }): string {
               return formatSortTextCurrency(this.value);
             }
           }
@@ -246,7 +236,18 @@ const charts = computed(() => {
           type: 'spline',
           yAxis: 0,
           zIndex: 1,
-          data: correctedSalesData,
+          data: props.data.data_analytic.by_overview.lst_revenue_sale_monthly
+              .slice()
+              .map(
+                  (monthly: {
+                    sale: number,
+                    score?: number,
+                    by_platform?: { platform_id: number, sale: number }[]
+                  }) =>
+                      monthly.sale || (monthly.by_platform?.find(
+                          (p: { platform_id: number }) => Number(p.platform_id) === platformId
+                      )?.sale || monthly.score)
+              ),
         },
         {
           name: 'Doanh số',
@@ -275,7 +276,7 @@ const charts = computed(() => {
           },
           dataLabels: {
             enabled: true,
-            formatter: function(this: { y: number }): string {
+            formatter: function (this: { y: number }): string {
               return formatSortTextCurrency(this.y);
             },
             verticalAlign: 'top',
@@ -328,7 +329,7 @@ const charts = computed(() => {
     <InsightBlock v-else>
       <li>
         <b>
-          Doanh số bán {{data?.name}} trong {{ diffMonths }} đạt:
+          Doanh số bán {{ data?.name }} trong {{ diffMonths }} đạt:
           <BlurContent :is-hide-content="props.isHideContent">
         <span>
           <b>
@@ -349,7 +350,7 @@ const charts = computed(() => {
       </li>
       <li>
         Cập nhật tình hình thị trường {{ data?.name }} có hơn
-          <span>
+        <span>
             {{ formatNumber(data?.data_analytic?.by_overview?.shop) }}
           </span>
         nhà bán trên sàn TMĐT với hơn
@@ -365,14 +366,14 @@ const charts = computed(() => {
           :key="platform.name"
       >
         <b class="text-bold">{{ platform.name }}</b> chiếm
-          {{ Number(platform.ratio_revenue * 100).toFixed(1) }}%
+        {{ Number(platform.ratio_revenue * 100).toFixed(1) }}%
         tổng doanh số và
-          {{ Number(platform.ratio_sale * 100).toFixed(1) }}%
+        {{ Number(platform.ratio_sale * 100).toFixed(1) }}%
         về sản lượng
       </li>
       <li>
         Doanh số của sản phẩm {{ data?.name }} trong tháng
-          {{ formatDateFunc(hightestMonthRevenue.begin, "MM/YYYY") }}
+        {{ formatDateFunc(hightestMonthRevenue.begin, "MM/YYYY") }}
         đạt mức cao nhất với
         <BlurContent :is-hide-content="isHideContent">
           <span>
@@ -389,7 +390,8 @@ const charts = computed(() => {
       </li>
       <li>
         Quy mô thị trường {{ data?.name }} tháng
-        {{ diffRevenueMonths?.diffPercent !== undefined && diffRevenueMonths.diffPercent > 0 ? "tốt" : "thấp" }} hơn so với tháng        đạt
+        {{ diffRevenueMonths?.diffPercent !== undefined && diffRevenueMonths.diffPercent > 0 ? "tốt" : "thấp" }} hơn so
+        với tháng đạt
         <BlurContent :is-hide-content="isHideContent">
     <span>
       {{
@@ -400,9 +402,12 @@ const charts = computed(() => {
     </span>
         </BlurContent>
         doanh số và tăng trưởng
-        {{ diffRevenueMonths?.diffPercent !== undefined && diffRevenueMonths.diffPercent > 0 ? "tốt" : "thấp" }} hơn so với
+        {{ diffRevenueMonths?.diffPercent !== undefined && diffRevenueMonths.diffPercent > 0 ? "tốt" : "thấp" }} hơn so
+        với
         tháng
-        {{ diffRevenueMonths.previousMonth?.[0]?.begin ? formatDateFunc(diffRevenueMonths.previousMonth[0].begin, "MM/YYYY") : 'N/A' }}
+        {{
+          diffRevenueMonths.previousMonth?.[0]?.begin ? formatDateFunc(diffRevenueMonths.previousMonth[0].begin, "MM/YYYY") : 'N/A'
+        }}
         là {{ Math.abs(diffRevenueMonths?.diffPercent || 0) }}%.
       </li>
       <li>
@@ -417,7 +422,8 @@ const charts = computed(() => {
         <span>
           {{ diffHalfYear.diffPercent }}
         </span>
-        </BlurContent>% so với 6 tháng liền kề.
+        </BlurContent>
+        % so với 6 tháng liền kề.
       </li>
     </InsightBlock>
   </div>
