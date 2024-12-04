4d6127ad301254eafb871bb4fe34221a022e2ed4
cuongle <pau30012002@gmail.com>
2024-12-04 14:57:52 +0700

    updateTracking

--- services/tracking/TrackingEventService.js
+++ services/tracking/TrackingEventService.js
@@ -1,61 +1,33 @@
-import mixpanel from 'mixpanel-browser'
-import {getIndexedDB} from "~/helpers/IndexedDBHelper.js";
-import {removeEmpty} from "~/helpers/ObjectHelper.js";
-import {extractDomain} from "~/helpers/utils.js";
-import {getGlobalVariable} from "~/services/GlobalVariableService.js";
-import {apiSendTrackingData} from "~/services/tracking/ApiTrackingData.js";
-import {isClient} from "~/helpers/BrowserHelper.ts";
+// Update TrackingEventService.js to import the gtag function correctly
+import mixpanel from 'mixpanel-browser';
+import { getIndexedDB } from '~/helpers/IndexedDBHelper.js';
+import { removeEmpty } from '~/helpers/ObjectHelper.js';
+import { extractDomain } from '~/helpers/utils.js';
+import { getGlobalVariable } from '~/services/GlobalVariableService.js';
+import { apiSendTrackingData } from '~/services/tracking/ApiTrackingData.js';
+import { useNuxtApp } from '#app';
+import { isClient } from '~/helpers/BrowserHelper.ts';
+
+const { $gtag: gtag } = useNuxtApp();
 
 const shouldTrack = () => {
   if (isClient) {
-    // return true
-    let domain = extractDomain(document.location.href)
-    let variables = getGlobalVariable()
+    let domain = extractDomain(document.location.href);
+    let variables = getGlobalVariable();
     if (domain !== 'localhost' && !variables?.user_email?.includes('@metric.vn')) {
-      return true
+      return true;
     }
   }
-  return false
-}
-
-// const initTracking = () => {
-//   if (shouldTrack()) {
-//     let variables = getGlobalVariable()
-//     if (variables?.user_id) {
-//       gtag('set', {'user_id': variables.user_id});
-//       mixpanel?.identify?.(variables.user_id);
-//       window?.clarity?.("identify", variables.user_id)
-//     }
-//     let optionsDefault = {
-//       visitor_id: variables?.visitor_id,
-//       user_id: variables?.user_id,
-//       user_email: variables?.user_email,
-//       role: variables?.role,
-//       // url_referrer: variables?.url_referrer,
-//       // url: variables?.url,
-//       // route_name: variables?.route_name,
-//       utm_source: variables?.utm_source,
-//       utm_medium: variables?.utm_medium,
-//       utm_campaign: variables?.utm_campaign,
-//       utm_term: variables?.utm_term,
-//       utm_content: variables?.utm_content,
-//       pub: variables?.pub,
-//       is_mobile: variables?.is_mobile,
-//       // first_visit: variables?.first_visit,
-//     }
-//     gtag('set', 'user_properties', optionsDefault)
-//     mixpanel?.people?.set?.(optionsDefault)
-//     mixpanel?.people?.set_once?.({first_visit: variables?.first_visit})
-//   }
-// }
+  return false;
+};
 
 const setUserProperties = (options) => {
   if (shouldTrack()) {
-    let optionsMerged = removeEmpty(options)
-    gtag('set', 'user_properties', optionsMerged)
-    mixpanel?.people?.set?.(optionsMerged)
+    let optionsMerged = removeEmpty(options);
+    gtag('set', 'user_properties', optionsMerged);
+    mixpanel?.people?.set?.(optionsMerged);
   }
-}
+};
 
 const trackEventCommon = async (eventName, eventCategory, eventLabel = '', value = 1, params = null, isStoreApi = true) => {
   console.log('trackEventCommon called with:', { eventName, eventCategory, eventLabel, value, params, isStoreApi });
@@ -79,60 +51,7 @@ const trackEventCommon = async (eventName, eventCategory, eventLabel = '', value
   } else {
     console.log('Tracking is disabled');
   }
-}
-
-// const trackEventConversion = async (eventName, eventCategory, eventLabel = '', value = 1, params = null, isStoreApi = true) => {
-//   if (shouldTrack()) {
-//     await trackEventCommon(eventName, eventCategory, eventLabel, value, params, isStoreApi)
-//     window?.clarity?.("upgrade", eventName)
-//   }
-// }
-
-// const trackEventConversionPixel = async (eventName, content_category, content_ids, content_name, content_type, contents, currency, num_items, search_string, status, value = 1, phone = null, params = null, isStoreApi = true) => {
-//   if (shouldTrack()) {
-//     let trackParams = {}
-//     if (content_category) {
-//       trackParams.content_category = content_category
-//     }
-//     if (content_ids) {
-//       trackParams.content_ids = content_ids
-//     }
-//     if (content_name) {
-//       trackParams.content_name = content_name
-//     }
-//     if (content_type) {
-//       trackParams.content_type = content_type
-//     }
-//     if (contents) {
-//       trackParams.contents = contents
-//     }
-//     if (currency) {
-//       trackParams.currency = currency
-//     }
-//     if (num_items) {
-//       trackParams.num_items = num_items
-//     }
-//     if (search_string) {
-//       trackParams.search_string = search_string
-//     }
-//     if (status) {
-//       trackParams.status = status
-//     }
-//     if (value) {
-//       trackParams.value = value
-//     }
-//     let variables = getGlobalVariable()
-//     if (variables?.user_email) {
-//       trackParams.em = variables.user_email
-//     }
-//     if (phone) {
-//       trackParams.ph = formatPhoneVN(phone)
-//     }
-//     console.log('fbq: ', eventName, trackParams)
-//
-//     fbq?.('track', eventName, trackParams)
-//   }
-// }
+};
 
 const trackEventCustom = async (eventName, params, isStoreApi = true) => {
   console.log('trackEventCustom called with:', { eventName, params, isStoreApi });
@@ -144,7 +63,13 @@ const trackEventCustom = async (eventName, params, isStoreApi = true) => {
     const paramsEvent = { ...params, ...variables };
     console.log('Event details:', paramsEvent);
 
-    gtag?.('event', eventName, paramsEvent);
+    if (typeof gtag === 'function') {
+      console.log('Calling gtag with event:', eventName, paramsEvent);
+      gtag('event', eventName, paramsEvent);
+    } else {
+      console.warn('gtag function is not defined');
+    }
+
     mixpanel?.track?.(eventName, paramsEvent);
     window?.clarity?.("set", "event", eventName);
 
@@ -160,7 +85,7 @@ const trackEventCustom = async (eventName, params, isStoreApi = true) => {
       console.log('[dev] event', eventName, paramsEvent);
     }
   }
-}
+};
 
 const sendTrackingBehavior = async (dataTracking = {}, type = 'default') => {
   const timestamp = Date.now();
@@ -200,16 +125,7 @@ const sendTrackingBehavior = async (dataTracking = {}, type = 'default') => {
   return true;
 };
 
-// //mixpanel
-// const trackUserUnionProperties = async (options) => {
-//   mixpanel?.people?.union?.(options);
-// }
-
 export {
-  // initTracking,
   trackEventCommon,
-  setUserProperties,
-  // trackUserUnionProperties,
-  // trackEventConversion,
-  // trackEventConversionPixel
-}
+  setUserProperties
+};
