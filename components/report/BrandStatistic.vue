a3ae7408fbd64f5112d4667d109a003ee988e1df
cuongle <pau30012002@gmail.com>
2024-12-19 11:07:24 +0700

    update_conclude_brand

--- components/report/BrandStatistic.vue
+++ components/report/BrandStatistic.vue
@@ -309,19 +309,21 @@ const formattedBrandCount = computed(() => {
       "
     >
       <li>
-        Phân tích thị trường {{ data.name }} có hơn
-        <BlurContent :is-hide-content="isHideContent">
-          <span>
-            {{ formattedBrandCount }}
-          </span>
-        </BlurContent>
 
-        thương hiệu chiếm
-        {{
-          Number(
-              data.data_analytic.by_brand.ratio?.brand.ratio_revenue * 100 || 0
-          ).toFixed(1)
-        }}% tổng doanh thu. So sánh giữa 10 thương hiệu hàng đầu,
+<!--        Phân tích thị trường {{ data.name }} có hơn-->
+<!--        <BlurContent :is-hide-content="isHideContent">-->
+<!--          <span>-->
+<!--            {{ formattedBrandCount }}-->
+<!--          </span>-->
+<!--        </BlurContent>-->
+
+<!--        thương hiệu chiếm-->
+<!--        {{-->
+<!--          Number(-->
+<!--              data.data_analytic.by_brand.ratio?.brand.ratio_revenue * 100 || 0-->
+<!--          ).toFixed(1)-->
+<!--        }}% tổng doanh thu. -->
+        So sánh giữa 10 thương hiệu hàng đầu,
         <b class="text-bold">{{
             data.data_analytic.by_brand.lst_top_brand_revenue[0].name
           }}</b>
