e106fc6ba7321addc2f0ee6366b6572b07ec96ca
thdtt <thanhdat.vnu@gmail.com>
2025-02-06 17:43:50 +0700

    #posthog: add tracking

---
+++ plugins/posthog.client.js
@@ -0,0 +1,35 @@
+import { defineNuxtPlugin } from '#app'
+import posthog from 'posthog-js'
+export default defineNuxtPlugin(nuxtApp => {
+  const runtimeConfig = useRuntimeConfig();
+  const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
+    api_host: runtimeConfig.public.posthogHost || 'https://us.i.posthog.com',
+    capture_pageview: false, // we add manual pageview capturing below
+    loaded: (posthog) => {
+      if (import.meta.env.MODE === 'development') posthog.debug();
+    }
+  })
+
+  // Make sure that pageviews are captured with each route change
+  const router = useRouter();
+  router.afterEach((to, from) => {
+    nextTick(() => {
+      if (from.fullPath !== to.fullPath) {
+        posthog.capture('$pageleave', {
+          current_url: from.fullPath,
+        })
+      }
+
+      posthog.capture('$pageview', {
+        current_url: to.fullPath,
+      })
+    })
+  })
+
+  // return {
+  //   provide: {
+  //     posthog: () => posthogClient
+  //   }
+  // }
+  nuxtApp.provide('posthog', posthogClient)
+})
