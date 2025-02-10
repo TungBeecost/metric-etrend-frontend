2475d498017c2f4692e5c746a935ebd19b7d54c9
cuongle <pau30012002@gmail.com>
2025-02-10 14:00:39 +0700

    update_page_slug

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -302,6 +302,7 @@ const handleOk = () => {
 };
 
 const downloadSampleReport = () => {
+  trackEventCommon(EVENT_TYPE.CLICK_DOWNLOAD_SAMPLE, 'click_download_sample', '');
   const url = 'https://storage.googleapis.com/ereport-static/bao-cao-nganh-hang-sample.pdf'; // URL file cần tải xuống
   const fileName = 'Báo cáo mẫu.pdf'; // Tên file sẽ được lưu trên máy người dùng
   if (url) {
